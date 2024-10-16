3a85654ddcdd9647a99df225b2a5ac9f226986c5
Daffnel <3ggapp24.andrda@folkuniversitetet.nu>
2024-10-16 13:00:52 +0200

    Method to read alla Pokemons to  Array

---
+++ src/Data/FileHandling.java
@@ -0,0 +1,34 @@
+package Data;
+
+import Pokemon.Pokemon;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileHandling {
+
+    public static ArrayList<Pokemon> readPokemonData() throws IOException {
+
+        ;
+        Gson gson = new Gson();
+
+        ArrayList<Pokemon> pokemonList = null;
+        try (FileReader reader = new FileReader("pokemon.json")) {
+            Type pokemonListType = new TypeToken<ArrayList<Pokemon>>() {
+            }.getType();
+            pokemonList = gson.fromJson(reader, pokemonListType);
+          //  pokemonList.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return pokemonList;
+
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import Data.FileHandling;
+import Pokemon.Pokemon;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+
+        FileHandling.readPokemonData();  //testing
+
+
     }
 }
